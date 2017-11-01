5e3395708eb0b4207941eccf14e6195726b27333
Roman Golyshev <anstreth@gmail.com>
2017-11-02 02:30:29 +0300

    refactor python code a little

--- buffer/consumers.py
+++ buffer/consumers.py
@@ -1,19 +1,24 @@
 import json
 from channels.channel import Group
+from channels.message import Message
 
 
-def ws_connect(message):
+def ws_connect(message: Message):
     print("Connected")
     message.reply_channel.send({"accept": True})
     Group('chat').add(message.reply_channel)
 
 
-def ws_message(message):
+def ws_message(message: Message):
     print("Send message")
-    Group('chat').send({'text': json.dumps({'message': message.content['text'],
-                                            'sender': message.reply_channel.name})})
+    Group('chat').send({
+        'text': json.dumps({
+            'message': message.content['text'],
+            'sender': message.reply_channel.name
+        })
+    })
 
 
-def ws_disconnect(message):
+def ws_disconnect(message: Message):
     print("Disconnected")
     Group('chat').discard(message.reply_channel)
--- buffer/views.py
+++ buffer/views.py
@@ -2,18 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
-from buffer.models import Text
+from django.http import HttpResponse, HttpRequest
 
 
-def index(request):
-    #all_entries = Text.objects.all()
-    #print(all_entries)
+def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world! Buffers are waiting!")
 
 
-def read(request):
+def read(request: HttpRequest) -> HttpResponse:
     return render_to_response('read_template.html')
 
-def edit(request):
+
+def edit(request: HttpRequest) -> HttpResponse:
     return render_to_response('edit_template.html')
