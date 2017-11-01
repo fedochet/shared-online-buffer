e04de2abbdca37268fdb251c4851357d1d1c40f3
Ekaterina <katyacyfra@gmail.com>
2017-11-01 16:41:22 +0300

    Templates are now in /buffer/templates/ directory. Also added websocket for reader

--- buffer/views.py
+++ buffer/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from buffer.models import Text
 
@@ -10,3 +10,8 @@ def index(request):
     #all_entries = Text.objects.all()
     #print(all_entries)
     return HttpResponse("Hello, world! Buffers are waiting!")
+
+
+def read(request):
+    return render_to_response('read_template.html')
+
