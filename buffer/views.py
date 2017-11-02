50cf5fc8f711c6441e42f15b885cff270bb0f041
Ekaterina <katyacyfra@gmail.com>
2017-11-02 12:00:01 +0300

    BD queries. TODO: remove bd file from repo

--- buffer/views.py
+++ buffer/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from buffer.models import Text
 
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpRequest
@@ -7,11 +8,35 @@ from django.http import HttpResponse, HttpRequest
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world! Buffers are waiting!")
-
-
+  
+  
 def read(request: HttpRequest) -> HttpResponse:
     return render_to_response('read_template.html')
 
 
 def edit(request: HttpRequest) -> HttpResponse:
     return render_to_response('edit_template.html')
+
+def create(text, name):
+    '''Creates record in bd, 2 args - text and name. 
+       Returns id of new record!'''
+    new_text = Text(text = text, name = name)
+    new_text.save()
+    return new_text.id
+
+def get(id):
+    '''Reads record in bd. Returns dict with keys: id, text and name'''
+    get_text = Text.objects.get(id = id)
+    rec = {'id' : get_text.id, 
+           'text' : get_text.text,
+           'name' : get_text.name}
+    return(rec)
+
+def update(id, text, name):
+    '''Updates record in bd, 3 args - id, text and name'''
+    upd_text = Text(id = id, text = text, name = name)
+    upd_text.save()
+
+    
+    
+    
