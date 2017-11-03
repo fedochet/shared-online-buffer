8427f5aecc27d3b39a0942a82dd520da82fcb5b9
Ekaterina <katyacyfra@gmail.com>
2017-11-03 15:17:35 +0300

    error handlers + input limit

--- buffer/urls.py
+++ buffer/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^read/([a-zA-Z0-9]{6})$', views.read, name='read'),
-    url(r'^edit/([a-zA-Z0-9]{8})$', views.edit, name='edit'),
+    url(r'^read/([a-zA-Z0-9]*)$', views.read, name='read'),
+    url(r'^edit/([a-zA-Z0-9]*)$', views.edit, name='edit'),
     url(r'^new$', views.new, name='new')
 ]
--- buffer/views.py
+++ buffer/views.py
@@ -19,17 +19,22 @@ def index(request: HttpRequest) -> HttpResponse:
     return render_to_response('index.html', {'links': links})
   
   
-def read(request: HttpRequest, _) -> HttpResponse:
-    return render_to_response('read_template.html')
+def read(request: HttpRequest, public_token: str) -> HttpResponse:
+    if lookup_public(public_token):
+         return render_to_response('read_template.html')
+    else:
+         return server_error('Public key is not valid')
 
 
 def edit(request: HttpRequest, private_token: str) -> HttpResponse:
-    context = {'public_key': lookup_private(private_token).public}
-    return render_to_response('edit_template.html', context=context)
+    if lookup_private(private_token):
+         context = {'public_key': lookup_private(private_token).public}
+         return render_to_response('edit_template.html', context=context)
+    else:
+         return server_error('Private key is not valid')
 
 
 def new(request: HttpRequest) -> HttpResponse:
-    print("New buf")
     new_id = create('', 'newrecord')
     created_record = get(new_id)
     return redirect('/buffer/edit/' + created_record.private)
@@ -47,22 +52,30 @@ def create(text, name):
 
 def get(id):
     '''Reads record in bd. Returns dict with keys: id, text and name'''
-    get_text = Text.objects.get(id=id)
-    return get_text
-
+    try:
+        get_text = Text.objects.get(id=id)
+        return get_text
+    except:
+        return server_error("No such id")
+       
 
 def update(id, text, name):
     '''Updates record in bd, 3 args - id, text and name'''
-    upd_text = Text(id=id, text=text, name=name)
-    upd_text.save()
+    try:
+       upd_text = Text(id=id, text=text, name=name)
+       upd_text.save()
+    except:
+       return server_error("No such id")
 
 
 def lookup_private(token) -> Text:
-    obj = Text.objects.filter(private=token)
+    obj = Text.objects.filter(private=token)    
     if obj:
         return obj[0]
     else:
-        raise ValueError('Private key is not valid')
+        return None
+
+
 
 
 def lookup_public(token) -> Text:
@@ -70,11 +83,18 @@ def lookup_public(token) -> Text:
     if obj:
         return obj[0]
     else:
-        raise ValueError('Public key is not valid')
-
+        return None
+      
 
 def gen(n):
     return ''.join(random.choice(string.ascii_uppercase +
                                  string.ascii_lowercase +
                                  string.digits)
                    for _ in range(n))
+
+#Error handlers
+def server_error(msg):
+    response = render_to_response(
+    'errors/500.html', {'msg': msg})
+    response.status_code = 500
+    return response
